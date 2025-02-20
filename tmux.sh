232f17d368e69edc0ba48cc8d64e1914b6bc221b
Hunter McKay <huntmckay@gmail.com>
2025-02-20 15:20:02 +0000

    fixed smashed commands

--- tmux.sh
+++ tmux.sh
@@ -10,13 +10,13 @@ if [ $? != 0 ]; then
     tmux new-session -d -s $TESS -n "sixthpillar"
 
     tmux new-window -n nvim 
-    tmux send-keys -t $TESS:nvim "cd ~/sixthpillar"
+    tmux send-keys -t $TESS:nvim "cd ~/sixthpillar" C-m
     tmux send-keys -t $TESS:nvim "source venv/bin/activate" C-m
     tmux send-keys -t $TESS:nvim "cd backend" C-m
     tmux send-keys -t $TESS:nvim "vim app.py models.py" C-m
 
     tmux new-window -n app_run
-    tmux send-keys -t $TESS:app_run "cd ~/sixthpillar"
+    tmux send-keys -t $TESS:app_run "cd ~/sixthpillar" C-m
     tmux send-keys -t $TESS:app_run "source venv/bin/activate" C-m
     tmux send-keys -t $TESS:app_run "cd backend" C-m
 
