8164c92b7241327e77a596d0dfa37f1195533ce5
Hunter McKay <huntmckay@gmail.com>
2025-02-22 14:15:30 +0000

    cleaner db print for testing

--- backend/sample.sh
+++ backend/sample.sh
@@ -1,3 +1,9 @@
 sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from macrocycle'
+echo ""
+echo "---"
+echo ""
 sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from mesocycle'
+echo ""
+echo "---"
+echo ""
 sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from microcycle'
