507580503b5a1a4f94ff7cf67e7e9ef8b012d2e9
Hunter McKay <huntmckay@gmail.com>
2025-02-28 15:19:55 +0000

    sample file update

--- backend/sample.sh
+++ backend/sample.sh
@@ -1,13 +1,17 @@
-sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from macrocycle'
-echo ""
-echo "---"
-echo ""
-sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from mesocycle'
-echo ""
-echo "---"
-echo ""
-sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from microcycle'
-echo ""
-echo "---"
-echo ""
-sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from tracker'
+# sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from macrocycle'
+# echo ""
+# echo "---"
+# echo ""
+# sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from mesocycle'
+# echo ""
+# echo "---"
+# echo ""
+# sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from microcycle'
+# echo ""
+# echo "---"
+# echo ""
+# sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from tracker'
+
+sqlite3 -cmd '.headers on' -cmd '.mode markdown' database.db 'select * from exercise'
+
+echo "You should add this to bash source so you can type it anywhere in the env"
